79c5ad52c017c9bda66c4e30cddb6788d777d9c0
YizeWang <yizwang@student.ethz.ch>
2019-08-30 11:59:52 +0200

    rescaled axes and made plotSample more robust
    set the same scale for y and x axis.
    fix plotting bugs when no feasible trajectory is available

--- Double Slit Example 2D/plotSample.m
+++ Double Slit Example 2D/plotSample.m
@@ -18,16 +18,20 @@ colorMat = jet(numcolor);
 [~,~,bin] = histcounts(sampleWeight,linspace(0,1,numcolor));
 
 %% plot truncated samples
-for n = 1:size(feasiblePath,3)
-    plot3(simHorizon,feasiblePath(1,:,n),feasiblePath(2,:,n),'color',colorMat(bin(n),:));
-    hold on
+if ~isempty(feasiblePath)
+    for n = 1:size(feasiblePath,3)
+        plot3(simHorizon,feasiblePath(1,:,n),feasiblePath(2,:,n),'color',colorMat(bin(n),:));
+        hold on
+    end
 end
-for n = 1:size(inFeasiblePath,3)
-    plot3(simHorizon(1:barrierStep),inFeasiblePath(1,1:barrierStep,n),inFeasiblePath(2,1:barrierStep,n),'color','k');
-    hold on
+if ~isempty(inFeasiblePath)
+    for n = 1:size(inFeasiblePath,3)
+        plot3(simHorizon(1:barrierStep),inFeasiblePath(1,1:barrierStep,n),inFeasiblePath(2,1:barrierStep,n),'color','k');
+        hold on
+    end
 end
 colormap(jet);
-colorbar('Ticks',[0 1],'TickLabels',{'Low Weight','High Weight'},'fontsize',param.fontSize);
+colorbar('west','Ticks',[0 1],'TickLabels',{'Low Weight','High Weight'});
 
 %% plot barrier
 fill3(param.barrierTime*param.barrierX,param.barrierSide*param.barrierY,param.barrierSide*param.barrierZ,'k');
@@ -39,6 +43,8 @@ title("Sample Visualization"+" "+"(Pass Rate:"+" "+num2str(passRate,3)+"%)",'fon
 xlabel("Time",'fontsize',param.fontSize)
 ylabel("x_1",'fontsize',param.fontSize)
 zlabel("x_2",'fontsize',param.fontSize)
+axisLimit = axis;
+axis ([0 param.simEnd -max(abs(axisLimit(3:6))) max(abs(axisLimit(3:6))) -max(abs(axisLimit(3:6))) max(abs(axisLimit(3:6)))])
 hold on
 
 %% plot initial state
--- Double Slit Example 2D/runSimulation.m
+++ Double Slit Example 2D/runSimulation.m
@@ -9,7 +9,7 @@ initState = [0;0];
 constraintType = 2;
 param.barrierSide = 0.2;
 param.stateInputConstraint_e = @(x)[-abs(x(2));-abs(x(2))];
-param.stateInputConstraint_F = @(x)[-10 0;10 0];
+param.stateInputConstraint_F = @(x)[-20 0;20 0];
 param.inputConstraint = [1 1;-1 -1];
 param.numSample = 20;
 param.barrierZ = [10;-10;-10;10];
@@ -49,7 +49,7 @@ for currentTime = param.simStart:param.simInterval:param.simEnd-param.simInterva
     end
     [cost,psi] = computeCost(trajectory,isBarrierDetected,param);
     if psi == 0 % if psi 0, use previous control
-        fprintf("Warning: All Samples Crashed at"+num2str(currentTime)+"\n");
+        fprintf("Warning: All Samples Crashed at "+num2str(currentTime)+"\n");
     else % if psi !0, compute new control with path integral
         u = computeU(cost,psi,noiseInput,param);
     end
@@ -87,17 +87,19 @@ hold on
 % plot barrier
 fill3(param.barrierTime*param.barrierX,param.barrierSide*param.barrierY,param.barrierSide*param.barrierZ,'black');
 hold on
-title("Actual Path")
-xlabel("Time")
-ylabel("x_1")
-zlabel("x_2")
+title("Actual Path",'fontsize',param.fontSize)
+axisLimit = 1.5*param.barrierSide*max(abs([param.barrierY(:);param.barrierZ(:)]));
+axis ([0 param.simEnd -axisLimit axisLimit -axisLimit axisLimit])
+xlabel("Time",'fontsize',param.fontSize)
+ylabel("x_1",'fontsize',param.fontSize)
+zlabel("x_2",'fontsize',param.fontSize)
 hold on
 
 % plot control input sequence
 subplot(2,2,3)
-title("|x_2| and 10u_1")
-xlabel("Time")
-ylabel("|x_2| or 10u_1")
+title("|x_2| and 10u_1",'fontsize',param.fontSize)
+xlabel("Time",'fontsize',param.fontSize)
+ylabel("|x_2| or 10u_1",'fontsize',param.fontSize)
 hold on
 plot(param.simStart:param.simInterval:param.simEnd-param.simInterval,10*U(1,:))
 hold on
@@ -105,13 +107,13 @@ plot(param.simStart:param.simInterval:param.simEnd-param.simInterval,abs(actualP
 hold on
 plot(param.simStart:param.simInterval:param.simEnd-param.simInterval,-abs(actualPath(2,1:end-1)),'color','r','LineWidth',0.5)
 hold on
-legend('10u_1','|x_2|','-|x_2|')
+legend('20u_1','+/-|x_2|','fontsize',param.fontSize)
 
 % plot optimal cost
 subplot(2,2,4)
-title("Optimal Cost")
-xlabel("Time")
-ylabel("Optimal Cost")
+title("Optimal Cost",'fontsize',param.fontSize)
+xlabel("Time",'fontsize',param.fontSize)
+ylabel("Optimal Cost",'fontsize',param.fontSize)
 axis([param.simStart param.simEnd 0 1.2*max(J)])
 hold on
 plot(param.simStart:param.simInterval:param.simEnd-param.simInterval,J)
