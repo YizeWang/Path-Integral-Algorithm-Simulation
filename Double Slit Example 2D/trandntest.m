443b691b5204350b3dd0ff95e56e66474c1ec891
YizeWang <yizwang@student.ethz.ch>
2019-09-02 12:55:58 +0200

    implementation better illustration of trandn

--- Double Slit Example 2D/trandntest.m
+++ Double Slit Example 2D/trandntest.m
@@ -1,7 +1,25 @@
 num = 1e6;
-uMax =  1*inf;
-uMin = -1*inf;
-uMaxVec = uMax*ones(num,1);
-uMinVec = uMin*ones(num,1);
-u = trandn(uMinVec,uMaxVec);
-histogram(u);
+uMax =  1;
+uMin = -1;
+edge = linspace(-5,5,400);
+figure('Name','Truncated Normal Distribution')
+subplot(2,2,1)
+u = trandn(-1*inf(num,1),1*inf(num,1));
+histogram(u,edge)
+title("No Bounds")
+axis([-5 5 0 15000]);
+subplot(2,2,2)
+u = trandn(-1*inf(num,1),uMax*ones(num,1));
+histogram(u,edge)
+title("Upper Bound 1")
+axis([-5 5 0 15000]);
+subplot(2,2,3)
+u = trandn(uMin*ones(num,1),1*inf(num,1));
+histogram(u,edge);
+title("Lower Bound -1")
+axis([-5 5 0 15000])
+subplot(2,2,4)
+u = trandn(uMin*ones(num,1),uMax*ones(num,1));
+histogram(u,edge);
+title("Bounded in [-1,1]")
+axis([-5 5 0 15000])
